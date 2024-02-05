0c716f79010f04e81c110b0cc56e42dcb310df2e
Sanfecker <sanfecker@gmail.com>
2024-02-05 19:45:43 +0100

    Initial Commit

---
+++ trivago_test/trivago_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  trivago_testApp.swift
+//  trivago_test
+//
+//  Created by Patrick on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct trivago_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
